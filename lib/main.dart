ef9ea633acd87417bb26619810d5e8d5676f3a79
Kostiantyn Makrasnov <32151128+makrasnov100@users.noreply.github.com>
2020-02-22 19:00:17 -0800

    Added stay in Portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; //force portrait up
 
 //Other Created Pages
 import 'entryPage.dart';
@@ -15,6 +16,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+          SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
+
     return MaterialApp(
       title: 'Campus Compass',
       theme: ThemeData(
